3fec4d14b73358579f6842b97d7bdc3c7e9bacd8
sumeethajbk <sumeetha@jbkinfotech.com>
2025-05-23 21:49:21 +0530

    commot

---
+++ website/js/custom-sticky.js
@@ -0,0 +1,4 @@
+<script src="https://cdn.jsdelivr.net/npm/sticksy@0.2.0/dist/sticksy.min.js"></script>
+jQuery(document).on('ready', function () {
+  jQuery('.js-sticky-widget').sticksy({ topSpacing: 60, listen: true });
+});
